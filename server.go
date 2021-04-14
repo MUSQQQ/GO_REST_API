06cb3246f151883cf981965b9ce3e6cb7163c405
Jan Walczak <super.walczak@gmail.com>
2021-04-14 21:51:34 +0200

    Add species handler

--- server.go
+++ server.go
@@ -2,7 +2,37 @@ package main
 
 import "net/http"
 
-func main(){
-    http.ListenAndServe(":8000", nil)
+type Species struct{
+    ID string `json:"id"`
+    GenericName string `json:"genericname"`
+    SpecificName string `json:"specificname"`
+    WorkersLength float64 `json:"workerslength"`
+    QueenLength float64 `json:"queenlength"`
+}
 
+type speciesHandlers struct{
+
+    store map[string]Species
+
+}
+func (h *speciesHandlers) get(w http.ResponseWriter, r *http.Request){
+    
+}
+
+func newSpeciesHandlers() *speciesHandlers{
+
+    return &speciesHandlers{
+        store: map[string]Species{
+            
+        },
+    }
+}
+
+func main(){
+    speciesHandlers := newSpeciesHandlers()
+    http.HandleFunc("/species", speciesHandlers.get)
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil{
+        panic(err)
+    }
 }

5768296e4bfcdf0f1ec8d2076afec0675e335d68
Jan Walczak <super.walczak@gmail.com>
2021-04-21 10:50:34 +0200

    Finish GET species request

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import "net/http"
+import "encoding/json"
 
 type Species struct{
     ID string `json:"id"`
@@ -17,13 +18,36 @@ type speciesHandlers struct{
 }
 func (h *speciesHandlers) get(w http.ResponseWriter, r *http.Request){
     
+    speciesList := make([]Species, len(h.store))
+    
+    i := 0
+    for _, species := range h.store {
+        speciesList[i] = species
+        i++
+    }
+
+    jsonBytes, err := json.Marshal(speciesList)
+    if err != nil{
+        w.WriteHeader(http.StatusInternalServerError)
+        w.Write([]byte(err.Error()))
+    }
+    
+    w.Header().Add("content-type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    w.Write(jsonBytes)
 }
 
 func newSpeciesHandlers() *speciesHandlers{
 
     return &speciesHandlers{
         store: map[string]Species{
-            
+            "id1": Species{
+                ID: "id1",
+                GenericName: "Myrmica",
+                SpecificName: "rubra",
+                WorkersLength: 6,
+                QueenLength: 7.5,
+            },
         },
     }
 }

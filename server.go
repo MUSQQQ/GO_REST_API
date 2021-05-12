08b9040b0bf24df46486f39e035a5ae1e4bf1308
Jan Walczak <super.walczak@gmail.com>
2021-05-12 16:12:40 +0200

    Add GET /species/{id} request handling

--- server.go
+++ server.go
@@ -1,114 +1,144 @@
 package main
 
-import "net/http"
-import "encoding/json"
-import "sync"
-import "io/ioutil"
-import "time"
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
+)
 
-type Species struct{
-    ID string `json:"id"`
-    GenericName string `json:"genericname"`
-    SpecificName string `json:"specificname"`
-    WorkersLength float64 `json:"workerslength"`
-    QueenLength float64 `json:"queenlength"`
+type Species struct {
+	ID            string  `json:"id"`
+	GenericName   string  `json:"genericname"`
+	SpecificName  string  `json:"specificname"`
+	WorkersLength float64 `json:"workerslength"`
+	QueenLength   float64 `json:"queenlength"`
 }
 
-type speciesHandlers struct{
-    sync.Mutex
-    store map[string]Species
-
-}
-func (h *speciesHandlers) get(w http.ResponseWriter, r *http.Request){
-
-    speciesList := make([]Species, len(h.store))
-
-    h.Lock()
-    i := 0
-    for _, species := range h.store {
-        speciesList[i] = species
-        i++
-    }
-    h.Unlock()
-
-    jsonBytes, err := json.Marshal(speciesList)
-    if err != nil{
-        w.WriteHeader(http.StatusInternalServerError)
-        w.Write([]byte(err.Error()))
-        return
-    }
-
-    w.Header().Add("content-type", "application/json")
-    w.WriteHeader(http.StatusOK)
-    w.Write(jsonBytes)
+type speciesHandlers struct {
+	sync.Mutex
+	store map[string]Species
 }
 
-func (h *speciesHandlers) post(w http.ResponseWriter, r *http.Request){
-    bodyBytes, err := ioutil.ReadAll(r.Body)
-    defer r.Body.Close()
-    if err != nil{
-        w.WriteHeader(http.StatusInternalServerError)
-        w.Write([]byte(err.Error()))
-        return
-    }
+func (h *speciesHandlers) get(w http.ResponseWriter, r *http.Request) {
 
-    ct := r.Header.Get("content-type")
-    if ct != "application/json" {
-        w.WriteHeader(http.StatusUnsupportedMediaType)
-        w.Write([]byte(fmt.Sprintf("need content-type 'application/json', but got '%s'", ct)))
-        return
-    }
-    var species Species
-    err = json.Unmarshal(bodyBytes, &species)
-    if err != nil{
-        w.WriteHeader(http.StatusBadRequest)
-        w.Write([]byte(err.Error()))
-        return
-    }
+	speciesList := make([]Species, len(h.store))
 
-    species.ID = fmt.Sprintf("%d", time.Now().UnixNano())
+	h.Lock()
+	i := 0
+	for _, species := range h.store {
+		speciesList[i] = species
+		i++
+	}
+	h.Unlock()
 
-    h.Lock()
-    h.store[species.ID] = species
-    defer h.Unlock()
+	jsonBytes, err := json.Marshal(speciesList)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
 }
 
-func (h *speciesHandlers) species(w http.ResponseWriter, r *http.Request){
-    switch r.Method{
-    case "GET":
-        h.get(w,r)
-        return
-    case "POST":
-        h.post(w,r)
-        return
-    default:
-        w.WriteHeader(http.StatusMethodNotAllowed)
-        w.Write([]byte("method not allowed"))
-        return
-    }
+func (h *speciesHandlers) getSpecies(w http.ResponseWriter, r *http.Request) {
+
+	parts := strings.Split(r.URL.String(), "/")
+	if len(parts) != 3 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	h.Lock()
+	species, ok := h.store[parts[2]]
+	h.Unlock()
+
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	jsonBytes, err := json.Marshal(species)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
 }
 
-func newSpeciesHandlers() *speciesHandlers{
+func (h *speciesHandlers) post(w http.ResponseWriter, r *http.Request) {
+	bodyBytes, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
 
-    return &speciesHandlers{
-        store: map[string]Species{
-            "id1": Species{
-                ID: "id1",
-                GenericName: "Myrmica",
-                SpecificName: "rubra",
-                WorkersLength: 6,
-                QueenLength: 7.5,
-            },
-        },
-    }
+	ct := r.Header.Get("content-type")
+	if ct != "application/json" {
+		w.WriteHeader(http.StatusUnsupportedMediaType)
+		w.Write([]byte(fmt.Sprintf("need content-type 'application/json', but got '%s'", ct)))
+		return
+	}
+	var species Species
+	err = json.Unmarshal(bodyBytes, &species)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	species.ID = fmt.Sprintf("%d", time.Now().UnixNano())
+
+	h.Lock()
+	h.store[species.ID] = species
+	defer h.Unlock()
 }
 
-func main(){
-    speciesHandlers := newSpeciesHandlers()
-    http.HandleFunc("/species", speciesHandlers.species)
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil{
-        panic(err)
-    }
+func (h *speciesHandlers) species(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		h.get(w, r)
+		return
+	case "POST":
+		h.post(w, r)
+		return
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("method not allowed"))
+		return
+	}
 }
+
+func newSpeciesHandlers() *speciesHandlers {
+
+	return &speciesHandlers{
+		store: map[string]Species{},
+	}
+}
+
+func main() {
+	speciesHandlers := newSpeciesHandlers()
+	http.HandleFunc("/species", speciesHandlers.species)
+	http.HandleFunc("/species/", speciesHandlers.getSpecies)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+/* json input data for testing
+
+curl localhost:8080/species -X POST -d '{"genericname": "Lasius", "specificname": "niger", "workerslength": 4, "queenlength" : 9}' -H "Content-Type: application/json"
+
+*/
